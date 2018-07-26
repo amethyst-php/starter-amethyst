00343f63a33670fc2647cb81628fc79999297d8e
railken <railken@protonmail.ch>
2018-07-26 22:32:16 +0200

    add config auth

---
+++ config/ore.auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Entity
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the entity user used for authentication
+    |
+    */
+    'entity' => App\User::class,
+];
