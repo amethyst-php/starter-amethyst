092ad50c0668390e5c78d46b6ae836decc10fdf1
railken <railken@protonmail.ch>
2018-10-13 17:59:24 +0200

    fixes

--- config/app.php
+++ config/app.php
@@ -154,9 +154,10 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\AmethystServiceProvider::class,
     ],
 
     /*
