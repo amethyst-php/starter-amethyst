36db9c4d4d7c4efbd4ddc34db6f684036381ac65
railken <railken@protonmail.ch>
2018-10-24 00:12:06 +0200

    add api IndexController

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Collection;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,50 +12,4 @@ use Illuminate\Support\Collection;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::name('index')->get('/', function (Request $request) {
-
-	$user = $request->user('api');
-	// $user = Auth::user();
-
-
-	$endpoints = Collection::make(Route::getRoutes())
-	->filter(function ($route) use ($user) {
-		$middleware = isset($route->action['middleware']) ? $route->action['middleware'] : null;
-
-
-		if (!is_array($middleware)) {
-			$middleware = [$middleware];
-		}
-
-		if (!in_array('api', $middleware) && !in_array('auth:api', $middleware)) {
-			return false;
-		}
-
-		if (in_array('auth:api', $middleware) && $user == null) {
-			return false;
-		}
-
-		if (in_array('admin', $middleware) && $user->role !== 'admin') {
-			return false;
-		}
-
-		return true;
-	})
-	->sortBy(function($route) {
-		return $route->uri;
-	})
-	->map(function($route) {
-		return [
-			'methods' => $route->methods,
-			'uri' => $route->uri !== "/" ? "/".$route->uri : "/"
-		];
-	})->values()->toArray();
-
-    return [
-    	'name' => config('api.name'),
-    	'url' => config('api.url'),
-    	'description' => config('api.description'),
-    	'version' => config('api.version'),
-    	'endpoints' => $endpoints,
-    ];
-});
+Route::name('index')->get('/', 'IndexController@index');
